71d30fb4bcd4c2e4ee824d8102b257dda43e66dd
ilanach <ilana.cher@gmail.com>
2017-04-27 23:16:06 +0300

    start from drive

--- FinalProject/app/src/main/java/cs/finalproject/MainActivity.java
+++ FinalProject/app/src/main/java/cs/finalproject/MainActivity.java
@@ -1,7 +1,10 @@
 package cs.finalproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void ChangeToRegistrationActivity(View v){
+        Intent intent = new Intent(this,RegActivity.class);
+        startActivity(intent);
+    }
+
+    public void ChangeToStartActivity(View v){
+        Intent intent = new Intent(this,StartActivity.class);
+        startActivity(intent);
     }
 }
---
+++ FinalProject/app/src/main/java/cs/finalproject/StartActivity.java
@@ -0,0 +1,20 @@
+package cs.finalproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+
+    public void ChangeToQuestionActivity(View v){
+        Intent intent = new Intent(this,QuestionActivity.class);
+        startActivity(intent);
+    }
+}
